786040788a3b456a2028e18c8ad529e8e70c3c96
yur-sudo <wlswjd1332@gmail.com>
2026-04-04 22:35:45 +0900

    feat: 메타데이터 가이드 및 스키마 추가

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# 샘플 Python 스크립트입니다.
-
-# ⌃R을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 ⇧ 두 번을(를) 누릅니다.
+from pathlib import Path
 
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 ⌘F8을(를) 누릅니다.
+# 메인 실행 함수
+def main():
+    # 운영체제에 상관없이 경로를 안전하게 생성함
+    # Path("폴더명") / "파일명" 형식을 사용함
+    data_path = Path("data") / "manual.pdf"
+
+    print(f"현재 설정된 파일 경로는: {data_path}")
 
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+# 프로그램 시작점
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+    main()
